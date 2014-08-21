7241d4917c55e28e56dde9a94d3729b534c259b5
David Lineberger <dlineberger@gmail.com>
2014-08-20 19:21:01 -0500

    Adding OpenShift support.

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+var db_host = 'localhost';
+var db_port = '27017';
+var db_name = 'vitals';
+
+if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
+	db_host = process.env.OPENSHIFT_MONGODB_DB_HOST;
+}
+if (process.env.OPENSHIFT_MONGODB_DB_PORT) {
+	db_port = process.env.OPENSHIFT_MONGODB_DB_PORT;
+}
+
+var mongodb_connection_string = 'mongodb://' + db_host + ':' + db_port + '/' + db_name;
+
 var _ = require('underscore');
 var express = require('express');
 var app = express();
@@ -10,7 +26,7 @@ require(__dirname + '/app/models');
 var Patient = mongoose.model('Patient');
 var Reading = mongoose.model('Reading');
 
-var db = mongoose.connect("mongodb://localhost/acme");
+var db = mongoose.connect(mongodb_connection_string);
 
 app.use(express.json());
 
@@ -21,7 +37,7 @@ app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/public/index.html');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(server_port, server_ip_address, function() {
 	socketio.listen(server);
 	console.log('Listening on port %d', server.address().port);
 });
